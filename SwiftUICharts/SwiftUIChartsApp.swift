3b935dad068dfc9730a112278eabaad5e5bae5de
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-29 13:47:05 -0600

    Initial Commit

---
+++ SwiftUICharts/SwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsApp.swift
+//  SwiftUICharts
+//
+//  Created by J. DeWeese on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
